4aabc9e8eb4f062d77d041dc946d5b9774620cda
Rys Sommefeldt <rys@sommefeldt.com>
2021-07-11 11:44:58 +0100

    Initial CU1 support

---
+++ keyboards/capsunlocked/cu1/keymaps/default/keymap.c
@@ -0,0 +1,45 @@
+/* Copyright 2021 Rys Sommefeldt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+	 * ┌───┐
+	 * │ A │
+	 * └───┘
+	 */
+	[0] = LAYOUT_ortho_1x1(
+		KC_A
+	)
+};
+
+// volume up/down on the encoder
+bool encoder_update_user(uint8_t index, bool clockwise) {
+	if (clockwise) {
+		tap_code_delay(KC_VOLU, 10);
+	}
+	else {
+		tap_code_delay(KC_VOLD, 10);
+	}
+
+	return true;
+}
+
+
+void keyboard_post_init_user(void) {
+  debug_enable=true;
+  debug_matrix=true;
+}
---
+++ keyboards/capsunlocked/cu1/readme.md
@@ -0,0 +1,16 @@
+# CU1
+
+The CU1 is the PCB for the CU1 rotary encoder pad.
+
+To place the keyboard into bootloader mode in order to flash it, use the reset button on the back side of the PCB.
+
+* Keyboard Maintainer: [rys](https://github.com/rys)
+* Hardware Supported: [CU1](url)
+* Hardware Availability: [CapsUnlocked](url)
+
+Make and flash examples for this keyboard (after setting up your build environment):
+
+    make capsunlocked/cu1:default
+    make capsunlocked/cu1:default:flash
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
